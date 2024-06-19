136b4f2362000ea62e2e3c2a662c2a2cca3ec82d
Samuel David Vasquez Solarte <sdvasquez@HT.LOCAL>
2024-06-19 18:34:15 -0500

    html_web

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+var terrariumElement ;
+
+function dragElement(element){
+let pos1=0,
+    pos2=0,
+    pos3=0,
+    pos4=0;
+element.onpointerdown=pointerDrag;
+terrariumElement=element;
+}
+
+function pointerDrag(e){
+    e.preventDefault();
+    console.log(e);
+    pos3=e.clientX;
+    pos4=e.clientY;
+
+    document.onpointermove=elementDrag;
+    document.onpointerup=stopElementDrag;
+}
+
+function elementDrag(e){
+    pos1=pos3 -e.clientX;
+    pos2=pos4-e.clientY;
+    pos3=e.clientX;
+    pos4=e.clientY;
+    console.log(pos1,pos2,pos3,pos4);
+    terrariumElement.style.top =terrariumElement.offsetTop -pos2 + 'px';
+    terrariumElement.style.left =terrariumElement.offsetLeft -pos1 + 'px';
+
+}
+
+function stopElementDrag (){
+    document.onpointerup= null;
+    document.onpointermove =null;
+
+}
+dragElement(document.getElementById('plant1'));
+dragElement(document.getElementById('plant2'));
+dragElement(document.getElementById('plant3'));
+dragElement(document.getElementById('plant4'));
+dragElement(document.getElementById('plant5'));
+dragElement(document.getElementById('plant6'));
